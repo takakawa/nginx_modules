9d8da4db3bebd9b1c6d32582a5a5aef189eb2afc
root <root@localhost.localdomain>
2018-02-03 02:07:55 -0500

    fix sigseg

--- ngx_test_module/ngx_test_module.c
+++ ngx_test_module/ngx_test_module.c
@@ -16,7 +16,15 @@ static ngx_command_t test_commands[] = {
 	},
 	ngx_null_command
 };
-
+static void * ngx_http_test_create_loc_conf(ngx_conf_t *cf)
+{
+	ngx_http_test_conf_loc_t *conf;
+	conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_test_conf_loc_t));
+	if(conf == NULL){
+		return NGX_CONF_ERROR;	
+	}
+	return conf;
+}
 static ngx_http_module_t test_ctx= {
 	NULL,
 	NULL,
@@ -24,7 +32,7 @@ static ngx_http_module_t test_ctx= {
 	NULL,
 	NULL,
 	NULL,
-	NULL,
+	ngx_http_test_create_loc_conf,
 	NULL
 };
 
@@ -38,28 +46,27 @@ ngx_module_t ngx_http_test_module = {
 	NGX_MODULE_V1_PADDING
 };
 static ngx_int_t handler(ngx_http_request_t *req){
-	u_char html[100] = "test";
+//	u_char html[100] = "test";
 	req->headers_out.status = 200;
-	int len = sizeof(html) -1;
-	req->headers_out.content_length_n = len;
-	ngx_str_set(&req->headers_out.content_type,"text/json");
-	ngx_http_send_header(req);
 
-	//ngx_http_test_conf_loc_t *mycnf;
-	//mycnf = ngx_http_get_module_loc_conf(req,ngx_http_test_module);
+	ngx_http_test_conf_loc_t *mycnf;
+	mycnf = ngx_http_get_module_loc_conf(req,ngx_http_test_module);
 	ngx_buf_t *b;
 	b = ngx_pcalloc(req->pool, sizeof(ngx_buf_t));
 	ngx_chain_t out;
 	out.buf = b;
 	out.next = NULL;
-	//b->pos = mycnf->hello.data;
-	//b->last = mycnf->hello.data+mycnf->hello.len;
+	b->pos = mycnf->hello.data;
+	b->last = mycnf->hello.data+mycnf->hello.len;
 	ngx_log_error(NGX_LOG_EMERG, req->connection->log, 0, "hello data");
-	b->pos = html;
-	b->last =html+len; 
+	//b->pos = html;
+	//b->last =html+len; 
 	b->memory =1;
 	b->last_buf = 1;
 
+	req->headers_out.content_length_n = mycnf->hello.len;
+	ngx_str_set(&req->headers_out.content_type,"text/json");
+	ngx_http_send_header(req);
 	return ngx_http_output_filter(req,&out);
 
 
